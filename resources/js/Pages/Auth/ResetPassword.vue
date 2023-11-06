7e919ce51bbc6e33f70247d58788a32661b20fa1
Nikhil Kerkar <nikhilkerkar43@gmail.com>
2023-11-06 11:59:54 +0530

    reset using password facade

--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,46 +1,169 @@
 <template>
-    <div>
-      <form @submit.prevent="submitForm">
-        <div class="mb-3">
-          <label for="email" class="form-label">Email address</label>
-          <input
-            v-model="email"
-            type="email"
-            class="form-control"
-            id="email"
-            name="email"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input
-            v-model="password"
-            type="password"
-            class="form-control"
-            id="password"
-            name="password"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="password_confirmation" class="form-label">Confirm Password</label>
-          <input
-            v-model="password_confirmation"
-            type="password"
-            class="form-control"
-            id="password_confirmation"
-            name="password_confirmation"
-          />
-        </div>
-        <input type="hidden" name="token" v-model="token" />
-        <button type="submit" class="btn btn-primary">Reset Password</button>
-      </form>
-    </div>
-  </template>
+  <div>
+    <form @submit.prevent="form.submit(submitForm)">
+      <input type="hidden" name="token" :value="token" />
+
+      <label for="email">Email:</label>
+      <input type="email" name="email" v-model="form.email" />
+
+      <label for="password">Password:</label>
+      <input type="password" name="password" v-model="form.password" />
+
+      <label for="password_confirmation">Confirm Password:</label>
+      <input type="password" name="password_confirmation" v-model="form.password_confirmation" />
+
+      <button type="submit">Reset Password</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import {  defineProps } from 'vue';
+import { useForm } from '@inertiajs/inertia-vue3'
+
+import { Inertia } from '@inertiajs/inertia';
+
+const props = defineProps({ token: String });
+
+const form =  useForm({
+  token: props.token,
+  email: '',
+  password: '',
+  password_confirmation: '',
+});
+
+const submitForm = () =>  form.post(
+  route('password.update'), form.data);
+
+</script>
+
+
+<!-- <template>
+
+
+works but directly posts to user reset table
+
+  <div>
+    <form @submit.prevent="submitForm">
+      <input type="hidden" name="token" :value="token" />
+
+      <label for="email">Email:</label>
+      <input type="email" name="email" v-model="email" />
+
+      <label for="password">Password:</label>
+      <input type="password" name="password" v-model="password" />
+
+      <label for="password_confirmation">Confirm Password:</label>
+      <input type="password" name="password_confirmation" v-model="password_confirmation" />
+
+      <button type="submit">Reset Password</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { useInertia, defineProps } from 'vue';
+
+const props = defineProps({ token: String });
+
+const email = ref('');
+const password = ref('');
+const password_confirmation = ref('');
+
+const submitForm = () => {
+  Inertia.post(route('password.update'), {
+    token: props.token,
+    email: email.value,
+    password: password.value,
+    password_confirmation: password_confirmation.value,
+  });
+};
+</script> -->
+
+
+
+
+
+
+
+
+
+<!-- <template>
+
+
+   this works with handling form but the post is not set in reset table 
+  <div>
+    <form @submit.prevent="submitForm">
+      <input type="hidden" name="token" :value="token" />
+
+      <label for="email">Email:</label>
+      <input type="email" name="email" v-model="email" />
+
+      <label for="password">Password:</label>
+      <input type="password" name="password" v-model="password" />
+
+      <label for="password_confirmation">Confirm Password:</label>
+      <input type="password" name="password_confirmation" v-model="password_confirmation" />
+
+      <button type="submit">Reset Password</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+
+
+const props = defineProps({ token: String })
+
+// console.log(props.token)
+
+const email = ref('');
+const password = ref('');
+const password_confirmation = ref('');
+
+const submitForm = () => {
+  Inertia.post(route('password.update'), {
+    token: props.token,
+    email: email.value,
+    password: password.value,
+    password_confirmation: password_confirmation.value,
+  });
+};
+</script>
+
+ -->
+
+
+
+ <!-- <template>
+  <div>
+    <form @submit.prevent="submitForm">
+      <input type="hidden" name="token" :value="token" />
+
+      <label for="email">Email:</label>
+      <input type="email" name="email" v-model="email" />
+
+      <label for="password">Password:</label>
+      <input type="password" name="password" v-model="password" />
+
+      <label for="password_confirmation">Confirm Password:</label>
+      <input type="password" name="password_confirmation" v-model="password_confirmation" />
+
+      <button type="submit">Reset Password</button>
+    </form>
+  </div>
+</template>
+
+
+ 
   
   <script>
   export default {
-    props: ['token'], // Receive the token as a prop
+    props: {
+      token: String,
+    },
     data() {
       return {
         email: '',
@@ -51,13 +174,13 @@
     methods: {
       submitForm() {
         this.$inertia.post(route('password.update'), {
+          token: this.token,
           email: this.email,
           password: this.password,
           password_confirmation: this.password_confirmation,
-          token: this.token,
         });
       },
     },
   };
-  </script>
+  </script> -->
   
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
-use Illuminate\Support\Str;
-use Inertia\Inertia;
-use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+use Inertia\Inertia;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\NotificationSeenController;
 // use Illuminate\Notifications\Events\NotificationSent;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Password;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\ListingOfferController;
 use App\Http\Controllers\RealtorListingAcceptOfferController;
@@ -18,6 +20,8 @@ use App\Http\Controllers\RealtorListingController;
 use App\Http\Controllers\RealtorListingImageController;
 use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -31,56 +35,104 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [IndexController::class, 'index']);
+
+
 // Route::get('/hello', [IndexController::class, 'show'])
 //   ->middleware('auth');
 
 Route::resource('listing', ListingController::class)
   ->only(['index', 'show']);
 
-  Route::resource('listing.offer',ListingOfferController::class)->middleware('auth')->only(['store']);
 
-  Route::resource('notification', NotificationController::class)
+Route::resource('listing.offer',ListingOfferController::class)
+  ->middleware('auth')
+  ->only(['store']);
+
+Route::resource('notification', NotificationController::class)
   ->middleware('auth')
   ->only(['index']);
 
-  Route::put(
-    'notification/{notification}/seen',
-    NotificationSeenController::class
-  )->middleware('auth')->name('notification.seen');
+Route::put('notification/{notification}/seen',NotificationSeenController::class)
+  ->middleware('auth')
+  ->name('notification.seen');
 
 Route::get('login', [AuthController::class, 'create'])
   ->name('login');
+
 Route::post('login', [AuthController::class, 'store'])
   ->name('login.store');
-Route::delete('logout', [AuthController::class, 'destroy'])
+  
+Route::delete('logout',[AuthController::class, 'destroy'])
   ->name('logout');
+
+
+//verify route template config
+Route::get('/email/verify',function(){
+    return inertia('Auth/VerifyEmail');
+  })
+  ->middleware('auth')
+  ->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect()
+    ->route('listing.index')
+    ->with('success','Email was Verified!');
+
+  })
+  ->middleware(['auth', 'signed'])
+  ->name('verification.verify');
+
+
+//resending verification email
+Route::post('/email/verification-notification', function (Request $request) {
+  $request->user()->sendEmailVerificationNotification();
+  return back()
+  ->with('success', 'Verification link sent!');
+
+  })
+  ->middleware(['auth', 'throttle:6,1'])
+  ->name('verification.send'); //throttle ->kernel.php 6 times in 1 min
+
 
 //forgot password
 // 1
 // Display password reset request form
-  Route::get('/forgot-password', function () {
-    return inertia('Auth/ForgotPassword');
-})->middleware('guest')->name('password.request');
+Route::get('/forgot-password', function () {
+  return inertia('Auth/ForgotPassword');
+  })
+  ->middleware('guest')
+  ->name('password.request');
 
 // 2
 // Handle password reset request
 Route::post('/forgot-password', function (Request $request) {
-  $request->validate(['email' => 'required|email']);
+  $request->validate(
+    ['email' => 'required|email|exists:users']);
 
-  $status = Password::sendResetLink(
-      $request->only('email')
-  );
+    $status = Password::sendResetLink(
+        $request->only('email')
+      );
 
   return $status === Password::RESET_LINK_SENT
-              ? back()->with(['status' => __($status)])
-              : back()->withErrors(['email' => __($status)]);
-})->middleware('guest')->name('password.email');
+              ? back()
+              ->with(['status' => __($status)])
+              : back()
+              ->withErrors(['email' => __($status)]);
+})
+  ->middleware('guest')
+  ->name('password.email');
 
 // 3
 // Display password reset form
-Route::get('/reset-password/{token}', function ($token) {
+Route::get('/reset-password/{token}', function (string $token) {
   return inertia('Auth/ResetPassword', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
+})
+->middleware('guest')
+->name('password.reset');
+
+
+
 
 // 4
 // Handle password reset form submission
@@ -90,25 +142,19 @@ Route::post('/reset-password', function (Request $request) {
       'email' => 'required|email',
       'password' => 'required|min:8|confirmed',
   ]);
-   // Debugging output
-   \Log::info('Token: ' . $request->input('token'));
-   \Log::info('Email: ' . $request->input('email'));
 
   $status = Password::reset(
       $request->only('email', 'password', 'password_confirmation', 'token'),
-      function ($user, $password) {
+      function (User $user, string $password) {
           $user->forceFill([
               'password' => Hash::make($password)
           ])->setRememberToken(Str::random(60));
 
           $user->save();
 
-          //output debug
-          \Log::info('Remember Token: ' . $user->getRememberToken());
           event(new PasswordReset($user));
       }
   );
-  \Log::info('Password Reset Status: ' . $status);
 
   return $status === Password::PASSWORD_RESET
               ? redirect()->route('login')->with('status', __($status))
@@ -116,39 +162,15 @@ Route::post('/reset-password', function (Request $request) {
 })->middleware('guest')->name('password.update');
 
 
-
-
-  //verify route template config
-  Route::get('/email/verify',function(){
-    return inertia('Auth/VerifyEmail');
-  })
-  ->middleware('auth')->name('verification.notice');
-
-  Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
- 
-    return redirect()->route('listing.index')->with('success','Email was Verified!');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-
-//resending verification email
-Route::post('/email/verification-notification', function (Request $request) {
-  $request->user()->sendEmailVerificationNotification();
-
-  return back()->with('success', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send'); //throttle ->kernel.php 6 times in 1 min
-
-  
-
 Route::resource('user-account', UserAccountController::class)
-  ->only(['create', 'store']);
-
+->only(['create', 'store']);
 
 
 Route::prefix('realtor')
   ->name('realtor.')
   ->middleware(['auth','verified'])
   ->group(function () {
+
     Route::name('listing.restore')
       ->put(
         'listing/{listing}/restore',
@@ -159,8 +181,9 @@ Route::prefix('realtor')
       // ->only(['index', 'destroy', 'edit', 'update', 'create', 'store'])
       ->withTrashed();
 
-//since we are modifying a data and not creating (put)
+      //since we are modifying a data and not creating (put)
     Route::name('offer.accept')->put('offer/{offer}/accept',RealtorListingAcceptOfferController::class);
 
-      Route::resource('listing.image',RealtorListingImageController::class)->only(['create','store','destroy']);
+    Route::resource('listing.image',RealtorListingImageController::class)->only(['create','store','destroy']);
+ 
   });
